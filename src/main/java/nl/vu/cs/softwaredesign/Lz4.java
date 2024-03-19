aae28d76ef0862cd033798338a280f72c254ba33
Jacob Roberts <slothsage@proton.me>
2024-03-19 16:45:37 +0100

    Draft of compStrat class

--- src/main/java/nl/vu/cs/softwaredesign/Lz4.java
+++ src/main/java/nl/vu/cs/softwaredesign/Lz4.java
@@ -9,11 +9,13 @@ public class Lz4 extends CompressionStrategy {
     }
 
     // Compresses a collection of objects into a ZIP archive format
+    @Override
     public Archive compress(Collection<?> collection) {
         // TODO: Implement the actual ZIP compression logic here
         return new Archive(); // Replace with actual implementation.
     }
     // Extracts the contents of a ZIP archive to a specified destination
+    @Override
     public void extractContents(String destination) {
         // TODO: Implement the actual ZIP extraction logic here. Not sure how to to do yet
     }

1f0b19721635490909dc4cea21952905b5c33151
aserbina28 <anna.r.serbina@gmail.com>
2024-03-29 11:49:56 +0100

    minor fix

--- src/main/java/nl/vu/cs/softwaredesign/Encryption.java
+++ src/main/java/nl/vu/cs/softwaredesign/Encryption.java
@@ -24,7 +24,7 @@ public class Encryption {
         archivesToPasswords.put(archiveId, password);
     }
 
-    public boolean validatePassword(int archiveId, String password) {
+    private boolean validatePassword(int archiveId, String password) {
         return archivesToPasswords.containsKey(archiveId) && archivesToPasswords.get(archiveId).equals(password);
     }
 
@@ -56,4 +56,22 @@ public class Encryption {
             Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(cipherMode, secretKey);
 
-            FileInputStream inputStream = 
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            byte[] inputBytes = new byte[(int) inputFile.length()];
+            inputStream.read(inputBytes);
+
+            byte[] outputBytes = cipher.doFinal(inputBytes);
+
+            FileOutputStream outputStream = new FileOutputStream(outputFile);
+            outputStream.write(outputBytes);
+
+            inputStream.close();
+            outputStream.close();
+
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException
+                 | InvalidKeyException | BadPaddingException
+                 | IllegalBlockSizeException | IOException | InvalidKeySpecException ex) {
+            throw new CryptoException("Error encrypting/decrypting file", ex);
+        }
+    }
+}

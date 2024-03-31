c61fcb6f5f9e14d0e8ac3598a08e574337334048
Jacob Roberts <slothsage@proton.me>
2024-03-31 19:30:59 +0200

    most recent stuff

--- src/main/java/nl/vu/cs/softwaredesign/Lz4.java
+++ src/main/java/nl/vu/cs/softwaredesign/Lz4.java
@@ -1,21 +1,55 @@
 package nl.vu.cs.softwaredesign;
 
-// public class Lz4 extends compressionStrategy {
+import net.jpountz.lz4.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
 
-    // Set compression level
-    // public Lz4 (int compressionLevel) {
-    //     this.compressionLevel = compressionLevel;
-    // }
+public class Lz4 extends CompressionStrategy {
 
-    // Compresses a collection of objects into a ZIP archive format
-    // @Override
-   // public Archive compress(nl.vu.cs.softwaredesign.Collection collection) {
-        // LZ4Factory factory = new
-        // return new Archive(); // Replace with actual implementation.
-    // }
-    // Extracts the contents of a ZIP archive to a specified destination
-    // @Override
-    // public void extractContents(Archive archive, String destination) {
-        // TODO: Implement the actual ZIP extraction logic here. Not sure how to to do yet
-    // }
-// }
+    public Lz4(int compressionLevel) {
+        this.compressionLevel = compressionLevel;
+    }
+
+    @Override
+    public void compress(HashMap<File, Metadata> fileMap, String destination) {
+        LZ4Factory factory = LZ4Factory.fastestInstance();
+        LZ4Compressor compressor = factory.fastCompressor();
+
+        try (ByteArrayOutputStream byteOutStream = new ByteArrayOutputStream()) {
+            for (File file : fileMap.keySet()) {
+                byte[] fileBytes = Files.readAllBytes(file.toPath());
+                byte[] compressed = compressor.compress(fileBytes);
+
+                // For simplicity, directly writing compressed bytes to the ByteArrayOutputStream
+                byteOutStream.write(compressed);
+            }
+
+            // Save compressed data to file
+            try (FileOutputStream fos = new FileOutputStream(destination)) {
+                fos.write(byteOutStream.toByteArray());
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void decompress(byte[] compressedData, String destination) {
+        LZ4Factory factory = LZ4Factory.fastestInstance();
+        LZ4FastDecompressor decompressor = factory.fastDecompressor();
+
+        try {
+
+            byte[] restored = decompressor.decompress(compressedData, compressedData.length);
+
+            try (FileOutputStream fos = new FileOutputStream(destination)) {
+                fos.write(restored);
+            }
+
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
